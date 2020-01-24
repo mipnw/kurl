1548132b897cf0820601c6cb5e78db9226a18b4c
mipnw <36454085+mipnw@users.noreply.github.com>
2020-01-24 13:06:22 -0800

    disable CGO in test and vet to match build

--- scripts/vet.sh
+++ scripts/vet.sh
@@ -3,7 +3,7 @@
 fmt=`go fmt ./...`
 [[ -n $fmt ]] && printf "go fmt error:\n$fmt\n" && exit 1
 
-vet=`go vet -mod=vendor ./...`
+vet=`env CGO_ENABLED=0 go vet -mod=vendor ./...`
 [[ -n $vet ]] && printf "go vet error:\n$vet\n" && exit 1
 
 exit 0
